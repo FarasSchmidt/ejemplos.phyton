e823f9119189a22272967692d41375e77861b012
FarasSchmidt <faras.a.schmidt@gmail.com>
2025-04-28 22:55:17 -0300

    subo tarea

--- 1er Cuatri2025/tareaWhile.py
+++ 1er Cuatri2025/tareaWhile.py
@@ -1,123 +1,123 @@
-# #Ejercicio 1
-# for i in range(0, 11):
-#     print(i)
+#Ejercicio 1
+for i in range(0, 11):
+    print(i)
 
-# #Ejercicio 2
-# for i in range(10, 0, -1):
-#     print(i)
+#Ejercicio 2
+for i in range(10, 0, -1):
+    print(i)
 
-# #Ejercicio 3
-# acumulador = 0
-# for i in range(0, 11):
-#     acumulador += i
-# print(acumulador)
+#Ejercicio 3
+acumulador = 0
+for i in range(0, 11):
+    acumulador += i
+print(acumulador)
 
-# #Ejercicio 4
-# acumulador = 0
-# for i in range(0, 11, 2):
-#     acumulador += i
-# print(acumulador)
+#Ejercicio 4
+acumulador = 0
+for i in range(0, 11, 2):
+    acumulador += i
+print(acumulador)
 
-# #Ejercicio 5
-# acumulador = 0
-# for i in range(0, 5):
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     acumulador += numero_ingresado
+#Ejercicio 5
+acumulador = 0
+for i in range(0, 5):
+    numero_ingresado = int(input("Ingrese un numero: "))
+    acumulador += numero_ingresado
 
-# promedio_acumulado = acumulador / 5
+promedio_acumulado = acumulador / 5
 
-# print(promedio_acumulado)
+print(promedio_acumulado)
 
-# #Ejercicio 6
-# contador = 0
-# acumulador = 0
+#Ejercicio 6
+contador = 0
+acumulador = 0
 
-# while True:
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     continua = str(input("Desea continuar? SI o NO: ")).lower()
-#     if continua == "no":
-#         break
-#     else:
-#         contador += 1
-#         acumulador += numero_ingresado
+while True:
+    numero_ingresado = int(input("Ingrese un numero: "))
+    continua = str(input("Desea continuar? SI o NO: ")).lower()
+    if continua == "no":
+        break
+    else:
+        contador += 1
+        acumulador += numero_ingresado
 
-# promedio_acumulado = acumulador / contador
+promedio_acumulado = acumulador / contador
 
-# print(f"La suma de los numeros es {acumulador} y el promedio es de {promedio_acumulado}")
+print(f"La suma de los numeros es {acumulador} y el promedio es de {promedio_acumulado}")
 
-# #Ejercicio 7
-# contador_negativos = 0
-# contador_positivos = 0
-# producto_negativos = 1
-# acumulador_positivos = 0
+#Ejercicio 7
+contador_negativos = 0
+contador_positivos = 0
+producto_negativos = 1
+acumulador_positivos = 0
 
-# while True:
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     continua = str(input("Desea continuar? SI o NO: ")).lower()
-#     if continua == "no":
-#         break
-#     else:
-#         if numero_ingresado > 0:
-#             acumulador_positivos += numero_ingresado
-#         else:
-#             producto_negativos *= numero_ingresado
+while True:
+    numero_ingresado = int(input("Ingrese un numero: "))
+    continua = str(input("Desea continuar? SI o NO: ")).lower()
+    if continua == "no":
+        break
+    else:
+        if numero_ingresado > 0:
+            acumulador_positivos += numero_ingresado
+        else:
+            producto_negativos *= numero_ingresado
 
-# if producto_negativos == 1:
-#     producto_negativos = 0
+if producto_negativos == 1:
+    producto_negativos = 0
 
 
 
-# print(f"La suma de los numeros positivos es de {acumulador_positivos}, El producto de los negativos es de {producto_negativos}")
+print(f"La suma de los numeros positivos es de {acumulador_positivos}, El producto de los negativos es de {producto_negativos}")
 
-# #Ejercicio 8
-# numero_minimo = float("inf")
-# numero_maximo = float("-inf")
-# for i in range(0, 10):
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     if numero_ingresado < numero_minimo:
-#         numero_minimo = numero_ingresado
-#     elif numero_ingresado > numero_maximo:
-#         numero_maximo = numero_ingresado
+#Ejercicio 8
+numero_minimo = float("inf")
+numero_maximo = float("-inf")
+for i in range(0, 10):
+    numero_ingresado = int(input("Ingrese un numero: "))
+    if numero_ingresado < numero_minimo:
+        numero_minimo = numero_ingresado
+    elif numero_ingresado > numero_maximo:
+        numero_maximo = numero_ingresado
 
-# print(f"El numero maximo es {numero_maximo} y el numero minimo es {numero_minimo}")
+print(f"El numero maximo es {numero_maximo} y el numero minimo es {numero_minimo}")
 
-# #Ejercicio 9
-# contador = 0
-# acumulador = 0
+#Ejercicio 9
+contador = 0
+acumulador = 0
 
-# while True:
-#     contador += 1
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     acumulador += numero_ingresado
+while True:
+    contador += 1
+    numero_ingresado = int(input("Ingrese un numero: "))
+    acumulador += numero_ingresado
 
-#     if contador > 4:
-#         continua = str(input("Desea continuar? SI o NO: ")).lower()
-#         if continua == "no":
-#             break
+    if contador > 4:
+        continua = str(input("Desea continuar? SI o NO: ")).lower()
+        if continua == "no":
+            break
 
-# promedio = acumulador / contador
+promedio = acumulador / contador
 
-# print(f"la suma de los numeros es {acumulador} y el promedio es {promedio}")
+print(f"la suma de los numeros es {acumulador} y el promedio es {promedio}")
 
-# #Ejercicio 10
-# contador = 0
-# acumulador = 0
+#Ejercicio 10
+contador = 0
+acumulador = 0
 
-# while True:
-#     contador += 1
-#     numero_ingresado = int(input("Ingrese un numero: "))
-#     acumulador += numero_ingresado
+while True:
+    contador += 1
+    numero_ingresado = int(input("Ingrese un numero: "))
+    acumulador += numero_ingresado
 
-#     if contador > 4 and contador < 9:
-#         continua = str(input("Desea continuar? SI o NO: ")).lower()
-#         if continua == "no":
-#             break
-#     elif contador > 9:
-#         break
+    if contador > 4 and contador < 9:
+        continua = str(input("Desea continuar? SI o NO: ")).lower()
+        if continua == "no":
+            break
+    elif contador > 9:
+        break
 
-# promedio = acumulador / contador
+promedio = acumulador / contador
 
-# print(f"la suma de los numeros es {acumulador} y el promedio es {promedio}")
+print(f"la suma de los numeros es {acumulador} y el promedio es {promedio}")
 
 acumulador_negativos = 0
 acumulador_positivos = 0
